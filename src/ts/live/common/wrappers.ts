7385ef3a27af2446c2ad4c181e7de567360dd546
yizhoucao <yizhou.cao@finbook.co>
2018-11-14 11:36:23 +0800

    minor change

--- src/ts/live/common/wrappers.ts
+++ src/ts/live/common/wrappers.ts
@@ -6,4 +6,4 @@ import * as CST from './constants';
 const provider =
 	(__KOVAN__ ? CST.PROVIDER_INFURA_KOVAN : CST.PROVIDER_INFURA_MAIN) + '/' + infura.token;
 export const web3Wrapper = new Web3Wrapper(window, '', provider, !__KOVAN__);
-export const beethovenWapper = new BeethovenWapper(web3Wrapper, !__KOVAN__);
+export const beethovenWapper = new BeethovenWapper(web3Wrapper);
