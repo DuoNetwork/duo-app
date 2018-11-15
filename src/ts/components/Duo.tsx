2222387cb63a4a56bd845c168dfa527e6b2c1f49
yizhoucao <yizhou.cao@finbook.co>
2018-11-15 19:35:29 +0800

    minor change

--- src/ts/components/Duo.tsx
+++ src/ts/components/Duo.tsx
@@ -22,6 +22,10 @@ export default class Duo extends React.Component {
 						render={() => <BeethovenAdmin tenor={CST.TH_PERPETUAL} />}
 					/>
 					<Route
+						path={'/beethoven/admin'}
+						render={() => <BeethovenAdmin tenor={CST.TH_PERPETUAL} />}
+					/>
+					<Route
 						path={'/beethoven/perpetual'}
 						render={() => <Beethoven tenor={CST.TH_PERPETUAL} />}
 					/>
