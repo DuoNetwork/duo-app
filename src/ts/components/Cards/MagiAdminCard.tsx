21f26ecbd3e76eaa67738174e92f67e70178eac1
WYANG008 <weitao.yang@finbook.co>
2019-02-01 15:16:21 +0800

    minor update

--- src/ts/components/Cards/MagiAdminCard.tsx
+++ src/ts/components/Cards/MagiAdminCard.tsx
@@ -6,7 +6,9 @@ import {} from 'ts/common/types';
 import { magiWrapper } from 'ts/common/wrappers';
 import { SDivFlexCenter } from '../_styled';
 import {
+	SCard,
 	SCardList,
+	SCardTitle,
 	// SCardExtraDiv,
 	SInput
 } from './_styled';
@@ -115,164 +117,200 @@ export default class MagiAdminCard extends React.Component<IProps, IState> {
 		console.log(isColdAddr);
 		const { priceFeedIndexErr, roleManagerErr } = this.state;
 		return (
-			<SDivFlexCenter>
-				<SCardList>
-					<div className="status-list-wrapper">
-						<ul>
-							<li>
-								<span className="title">{CST.TH_IS_STARTED}</span>
-								<span className="content">
-									{states.isStarted ? CST.TH_STARTED : CST.TH_NOT_STARTED}
-								</span>
-							</li>
-							<li>
-								<span className="title">{CST.TH_NUM_OF_PRICE[locale]}</span>
-								<span className="content">{states.numOfPrices}</span>
-							</li>
-							<li>
-								<span className="title">{CST.TH_PRICE_FEED_COOL_DOWN[locale]}</span>
-								<span className="content">
-									{states.priceUpdateCoolDown / 1000 / 60 + ' minutes'}
-								</span>
-							</li>
-							<li>
-								<span className="title">
-									{CST.TH_PRICE_FEED_TIME_TOLERANCE[locale]}
-								</span>
-								<span className="content">
-									{states.priceFeedTimeTolerance / 1000 + ' seconds'}
-								</span>
-							</li>
-							<li>
-								<span className="title">{CST.TH_PRICE_FEED_TOLERANCE[locale]}</span>
-								<span className="content">
-									{states.priceFeedTolerance * 100 + '%'}
-								</span>
-							</li>
-							<li>
-								<span className="title">{CST.TH_PRICE_TOLERANCE[locale]}</span>
-								<span className="content">{states.priceTolerance * 100 + '%'}</span>
-							</li>
-						</ul>
-					</div>
-				</SCardList>
-				<SCardList>
-					<div className="status-list-wrapper">
-						<ul>
-							{addresses.map((address, i) => (
-								<li className="block-title">
-									<span className="title">{CST.TH_PRICE_FEED[locale] + i}</span>
-									<span className="content">{address}</span>
+			<SCard
+				title={
+					<SCardTitle>
+						<a
+							className="tag-content"
+							href={
+								'https://' +
+								(__KOVAN__ ? 'kovan.' : '') +
+								'etherscan.io/address/' +
+								magiWrapper.web3Wrapper.contractAddresses.Oracles[0].address
+							}
+							target="_blank"
+							style={{ color: 'white' }}
+						>
+							{CST.TH_MAGI.toUpperCase()}
+						</a>
+					</SCardTitle>
+				}
+				width={'1000px'}
+				margin={'0 10px 0 10px'}
+			>
+				<SDivFlexCenter>
+					<SCardList>
+						<div className="status-list-wrapper">
+							<ul>
+								<li>
+									<span className="title">{CST.TH_IS_STARTED}</span>
+									<span className="content">
+										{states.isStarted ? CST.TH_STARTED : CST.TH_NOT_STARTED}
+									</span>
 								</li>
-							))}
-						</ul>
-					</div>
-				</SCardList>
-				<SCardList>
-					<div className="status-list-wrapper">
-						<ul>
-							<li className="no-bg">
-								<SDivFlexCenter
-									horizontal
-									width="100%"
-									padding="2px 0 2px 0"
-									marginBottom="10px"
-								>
-									{' '}
-									<SInput
-										className={priceFeedIndexErr ? 'input-error' : ''}
-										placeholder={CST.TT_INPUT_INDEX[locale]}
-										width={'400px'}
-										onChange={e =>
-											this.handlePriceFeedIndexChange(e.target.value)
-										}
-										small
-									/>
-									<button
-										className={'form-button'}
-										disabled={!isColdAddr}
-										onClick={() => this.handleUpdatePriceFeed()}
+								<li>
+									<span className="title">{CST.TH_NUM_OF_PRICE[locale]}</span>
+									<span className="content">{states.numOfPrices}</span>
+								</li>
+								<li>
+									<span className="title">
+										{CST.TH_PRICE_FEED_COOL_DOWN[locale]}
+									</span>
+									<span className="content">
+										{states.priceUpdateCoolDown / 1000 / 60 + ' minutes'}
+									</span>
+								</li>
+								<li>
+									<span className="title">
+										{CST.TH_PRICE_FEED_TIME_TOLERANCE[locale]}
+									</span>
+									<span className="content">
+										{states.priceFeedTimeTolerance / 1000 + ' seconds'}
+									</span>
+								</li>
+								<li>
+									<span className="title">
+										{CST.TH_PRICE_FEED_TOLERANCE[locale]}
+									</span>
+									<span className="content">
+										{states.priceFeedTolerance * 100 + '%'}
+									</span>
+								</li>
+								<li>
+									<span className="title">{CST.TH_PRICE_TOLERANCE[locale]}</span>
+									<span className="content">
+										{states.priceTolerance * 100 + '%'}
+									</span>
+								</li>
+							</ul>
+						</div>
+					</SCardList>
+					<SCardList>
+						<div className="status-list-wrapper">
+							<ul>
+								{addresses.map((address, i) => (
+									<li className="block-title">
+										<span className="title">
+											{CST.TH_PRICE_FEED[locale] + i}
+										</span>
+										<span className="content">{address}</span>
+									</li>
+								))}
+							</ul>
+						</div>
+					</SCardList>
+					<SCardList>
+						<div className="status-list-wrapper">
+							<ul>
+								<li className="no-bg">
+									<SDivFlexCenter
+										horizontal
+										width="100%"
+										padding="2px 0 2px 0"
+										marginBottom="10px"
 									>
-										{CST.TH_UPDATE_PRICE_FEED[locale]}
-									</button>
-								</SDivFlexCenter>
-							</li>
+										{' '}
+										<SInput
+											className={priceFeedIndexErr ? 'input-error' : ''}
+											placeholder={CST.TT_INPUT_INDEX[locale]}
+											width={'700px'}
+											onChange={e =>
+												this.handlePriceFeedIndexChange(e.target.value)
+											}
+											small
+										/>
+										<button
+											className={'form-button'}
+											disabled={!isColdAddr}
+											onClick={() => this.handleUpdatePriceFeed()}
+										>
+											{CST.TH_UPDATE_PRICE_FEED[locale]}
+										</button>
+									</SDivFlexCenter>
+								</li>
 
-							<li className="no-bg">
-								<SDivFlexCenter
-									horizontal
-									width="100%"
-									padding="2px 0 2px 0"
-									marginBottom="10px"
-								>
-									{' '}
-									<SInput
-										className={roleManagerErr ? 'input-error' : ''}
-										placeholder={CST.TT_INPUT_ADDR[locale]}
-										width={'400px'}
-										onChange={e => this.handleRoleManagerChange(e.target.value)}
-										small
-									/>
-									<button
-										className={'form-button'}
-										onClick={() => this.handleUpdateRoleManager()}
+								<li className="no-bg">
+									<SDivFlexCenter
+										horizontal
+										width="100%"
+										padding="2px 0 2px 0"
+										marginBottom="10px"
 									>
-										{CST.TH_UPDATE_ESP[locale]}
-									</button>
-								</SDivFlexCenter>
-							</li>
+										{' '}
+										<SInput
+											className={roleManagerErr ? 'input-error' : ''}
+											placeholder={CST.TT_INPUT_ADDR[locale]}
+											width={'700px'}
+											onChange={e =>
+												this.handleRoleManagerChange(e.target.value)
+											}
+											small
+										/>
+										<button
+											className={'form-button'}
+											onClick={() => this.handleUpdateRoleManager()}
+										>
+											{CST.TH_UPDATE_ESP[locale]}
+										</button>
+									</SDivFlexCenter>
+								</li>
 
-							<li className="no-bg">
-								<SDivFlexCenter
-									horizontal
-									width="100%"
-									padding="2px 0 2px 0"
-									marginBottom="10px"
-								>
-									{' '}
-									<SInput
-										className={roleManagerErr ? 'input-error' : ''}
-										placeholder={CST.TT_INPUT_INDEX[locale]}
-										width={'100px'}
-										onChange={e => this.handleSetValueIdxChange(e.target.value)}
-										small
-									/>
-									<SInput
-										className={roleManagerErr ? 'input-error' : ''}
-										placeholder={CST.TT_INPUT_VALUE[locale]}
-										width={'300px'}
-										onChange={e => this.handleSetValueChange(e.target.value)}
-										small
-									/>
-									<button
-										className={'form-button'}
-										onClick={() => this.handleSetValue()}
+								<li className="no-bg">
+									<SDivFlexCenter
+										horizontal
+										width="100%"
+										padding="2px 0 2px 0"
+										marginBottom="10px"
 									>
-										{CST.TH_SET_MAG_VALUE[locale]}
-									</button>
-								</SDivFlexCenter>
-							</li>
+										{' '}
+										<SInput
+											className={roleManagerErr ? 'input-error' : ''}
+											placeholder={CST.TT_INPUT_INDEX[locale]}
+											width={'200px'}
+											onChange={e =>
+												this.handleSetValueIdxChange(e.target.value)
+											}
+											small
+										/>
+										<SInput
+											className={roleManagerErr ? 'input-error' : ''}
+											placeholder={CST.TT_INPUT_VALUE[locale]}
+											width={'430px'}
+											onChange={e =>
+												this.handleSetValueChange(e.target.value)
+											}
+											small
+										/>
+										<button
+											className={'form-button'}
+											onClick={() => this.handleSetValue()}
+										>
+											{CST.TH_SET_MAG_VALUE[locale]}
+										</button>
+									</SDivFlexCenter>
+								</li>
 
-							<li className="no-bg">
-								<SDivFlexCenter
-									horizontal
-									width="100%"
-									padding="2px 0 2px 0"
-									marginBottom="10px"
-								>
-									<button
-										className={'form-button'}
-										disabled={!isColdAddr}
-										onClick={() => this.handleUpdateOperator()}
+								<li className="no-bg">
+									<SDivFlexCenter
+										horizontal
+										width="100%"
+										padding="2px 0 2px 0"
+										marginBottom="10px"
 									>
-										{CST.TH_UPDATE_OPERATOR[locale]}
-									</button>
-								</SDivFlexCenter>
-							</li>
-						</ul>
-					</div>
-				</SCardList>
-			</SDivFlexCenter>
+										<button
+											className={'form-button'}
+											disabled={!isColdAddr}
+											onClick={() => this.handleUpdateOperator()}
+										>
+											{CST.TH_UPDATE_OPERATOR[locale]}
+										</button>
+									</SDivFlexCenter>
+								</li>
+							</ul>
+						</div>
+					</SCardList>
+				</SDivFlexCenter>
+			</SCard>
 		);
 	}
 }
