0b1fabd6088168e5874319596cc26f3de15cfb60
WYANG008 <weitao.yang@finbook.co>
2019-03-05 11:43:46 +0800

    mc

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -25,7 +25,7 @@ module.exports = {
 	plugins: [
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify('production'),
-			__DEV__: true,
+			__DEV__: false,
 			__KOVAN__: false
 		}),
 		new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
