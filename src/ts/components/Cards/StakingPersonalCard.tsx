2daa826060afb2cf911ec7c31b2252d80e33cdcd
Sihao Wen <sihaowen@Sihaos-MacBook-Pro.local>
2019-07-10 08:20:42 +0800

    Minor style change

--- src/ts/components/Cards/StakingPersonalCard.tsx
+++ src/ts/components/Cards/StakingPersonalCard.tsx
@@ -315,7 +315,7 @@ export default class StakingPersonalCard extends React.Component<IProps, IState>
 									listStyle: 'none',
 									paddingLeft: 0,
 									width: '100%',
-									maxHeight: 200,
+									maxHeight: 240,
 									overflowY: 'scroll'
 								}}
 							>
